823af97f2951e15518c8b870ede299a6ca272012
kaizenNakamoto <kirankx4007@gmail.com>
2019-11-24 20:52:43 +0530

    Pick and drop basic

--- BKS.py
+++ BKS.py
@@ -3,37 +3,29 @@ import time
 G.setwarnings(False)
 G.cleanup()
 G.setmode(G.BOARD)
-G.setup(12,G.OUT)#in1
-G.setup(32,G.OUT)#in2
-G.setup(33,G.OUT)#in3
-G.setup(35,G.OUT)#in4
-G.setup(3,G.OUT)
 G.setup(5,G.OUT)
-mot1a=G.PWM(12,100)
-mot1b=G.PWM(32,100)
-mot2a=G.PWM(35,100)
-mot2b=G.PWM(33,100)
-
+G.setup(33,G.OUT)
+G.setup(35,G.OUT)
+G.setup(3,G.OUT)
+G.output(3,G.LOW)
+G.output(5,G.LOW)
+mot2=G.PWM(33,50) #arm
+mot3=G.PWM(35,50) #page
+mot2.start(0)
+mot3.start(0)
 def pg_rot(cc):
     if(cc==2):#counter clockwise
-         mot1a.start(10)
-         mot1b.start(0)
+         mot3.ChangeDutyCycle(2.5)
     elif cc==1:
-         mot1a.start(0)
-         mot1b.start(10)
+         mot3.ChangeDutyCycle(12.5)
     else:
-        mot1a.start(0)
-        mot1b.start(0)
-def arm_rot(cc):
-    if(cc==1):#counter clockwise
-         mot2a.start(10)
-         mot2b.start(0)
-    elif cc==2:
-         mot2a.start(0)
-         mot2b.start(10)
-    else:
-        mot2a.start(0)
-        mot2b.start(0)
+         mot3.ChangeDutyCycle(0)
+def arm_rot(ang):
+    pwm=ang/18+2.5
+    G.output(33,True)
+    mot2.ChangeDutyCycle(pwm)
+    time.sleep(0.3)
+    G.output(33,False)
 def pull(cmd):
     if cmd==1:
         G.output(3,G.HIGH)
@@ -41,17 +33,26 @@ def pull(cmd):
     else:
         G.output(3,G.LOW)
         G.output(5,G.LOW)
-pull(1)
+pull(0)
+count=0
+time.sleep(3)
 try:
     while(1):
-        arm_rot(1)
+        pull(1)
+        arm_rot(max(18-(count*0.3),1))
         time.sleep(2)
-        arm_rot(0)
-        time.sleep(1)
-        arm_rot(2)
-        time.sleep(2)
-        arm_rot(0)
-        time.sleep(1)
+        arm_rot(100)
+        pg_rot(2)
+        time.sleep(0.1)
+        pull(0)
+        time.sleep(0.5)
+        pg_rot(0)
+        time.sleep(0.2)
+        pg_rot(1)
+        time.sleep(0.6)
+        pg_rot(0)
+        time.sleep(0.3)
+        count=count+1
 
 except KeyboardInterrupt:
         print('out')
